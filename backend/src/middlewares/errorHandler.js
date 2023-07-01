a15eedfdeb95d8d31ac94bd114e3c15ad87c1da9
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-01 20:19:17 -0300

    bug: handler errors on errorHandler

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,23 +1,32 @@
 import { STATUS_CODES } from 'http';
 import { HttpException, HttpStatus } from 'http-exception-library';
 import { ZodError } from 'zod';
+import { loggerError } from '../logs/logger.js';
 
 export function errorHandler(error, req, res, next) {
-  if (error instanceof HttpException) {
-    return res.status(error.statusCode).json({
-      message: error.message,
-      statusCode: error.statusCode,
-      error: error.error,
-    });
+  try {
+    if (error instanceof HttpException) {
+      return res.status(error.statusCode).json({
+        message: error.message,
+        statusCode: error.statusCode,
+        error: error.error,
+      });
+    }
+
+    if (error instanceof ZodError) {
+      return res.status(HttpStatus.BAD_REQUEST).json(error);
+    }
+
+    loggerError('error.log').error(error);
+
+    return res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send(STATUS_CODES[HttpStatus.INTERNAL_SERVER_ERROR]);
+  } catch (error) {
+    loggerError('error.log').error(error);
+
+    return res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send(STATUS_CODES[HttpStatus.INTERNAL_SERVER_ERROR]);
   }
-
-  console.log(error);
-
-  if (error instanceof ZodError) {
-    return res.status(HttpStatus.BAD_REQUEST).json(error);
-  }
-
-  return res
-    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-    .send(STATUS_CODES[HttpStatus.INTERNAL_SERVER_ERROR]);
 }
