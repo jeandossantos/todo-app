f71b3e50d684bdd73316c4f98fb78639e3a0d6b3
Jean F. dos Santos <jeanddg@hotmail.com>
2023-08-12 20:06:07 -0300

    feat: create task validation

---
+++ backend/src/app/task/validation.js
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+const validateCreateTask = function (data) {
+  const schema = z.object({
+    user_id: z.string().uuid(),
+    title: z.string().min(2),
+    description: z.string().optional(),
+    priority: z.number().default(0),
+    deadline: z.string().refine(
+      (dateString) => {
+        const inputDate = new Date(dateString);
+        const currentDate = new Date();
+
+        if (isNaN(inputDate.getTime())) return false;
+
+        return inputDate > currentDate;
+      },
+      {
+        message:
+          'Deadline cannot be early than current date / deadline must be a UTC format date.',
+      }
+    ),
+  });
+
+  schema.parse(data);
+};
+
+export { validateCreateTask };
